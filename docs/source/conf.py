c33d6cd4e8280374c785863e4319b3187e82fdf5
systematic-magic <32806080+systematic-magic@users.noreply.github.com>
2017-10-16 16:06:43 +0800

    config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,11 +31,10 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
- extensions = [#'sphinx.ext.doctest',
+extensions = [#'sphinx.ext.doctest',
 #     'sphinx.ext.intersphinx',
 #     'sphinx.ext.todo',
-#     'sphinx.ext.coverage',
-#     'sphinx.ext.viewcode'
+#     'sphinx.ext.coverage'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
